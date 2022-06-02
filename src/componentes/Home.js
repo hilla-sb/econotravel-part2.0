a3fccb0a9e2caa4e5f0c0413b722150fe9e984cf
julie <j.u.veiga@hotmail.com>
2022-06-02 14:11:51 +0200

    esqueleto2

---
+++ src/componentes/Home.js
@@ -0,0 +1,73 @@
+import React from 'react';
+
+export default function Home () {
+  return (
+    <>
+      <img src="" alt="" href="">Imagen home</img>
+      <Button>Descubre más</Button>
+
+//---------------------------------------------------------------------
+      <h1>Experiencias en el área de Barcelona</h1>
+      <link>Ubicación</link> 
+      <link>Transporte</link>
+      <link>Duración</link> 
+
+//_____________________________---------------------_________________________
+      <div>
+        <img src="" alt="" href="">Exp 1</img>
+        <div>
+          <p>Etiqueta 1</p>
+          <p>Etiqueta 2</p>
+          <p>Etiqueta 3</p>
+        </div>
+      <div>
+          <p>Ubicación</p>
+          <p>Precio</p>    
+      </div>
+      <div>
+        <link>Reserva ahora</link>
+      </div>
+      </div>
+
+//__________________________________________________________________________
+      <div>
+        <img src="" alt="" href="">Exp 2</img>
+        <div>
+          <p>Etiqueta 1</p>
+          <p>Etiqueta 2</p>
+          <p>Etiqueta 3</p>
+        </div>
+      <div>
+          <p>Ubicación</p>
+          <p>Precio</p>    
+      </div>
+      <div>
+        <link>Reserva ahora</link>
+      </div>
+      </div>
+
+//__________________________________________________________________________
+
+      <div>
+        <img src="" alt="" href="">Exp 3</img>
+        <div>
+          <p>Etiqueta 1</p>
+          <p>Etiqueta 2</p>
+          <p>Etiqueta 3</p>
+        </div>
+      <div>
+          <p>Ubicación</p>
+          <p>Precio</p>    
+      </div>
+      <div>
+        <link>Reserva ahora</link>
+      </div>
+      </div>
+//---------------------------------------------------------------------
+    <div>
+      <img></img>
+    </div>
+    </>
+      )
+}
+   
