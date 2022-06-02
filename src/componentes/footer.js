4a8584a7d26cc68112de8f27f1353edcda530961
mada <madaagarcia77@gmail.com>
2022-06-02 13:00:49 +0200

    mada cambios

---
+++ src/componentes/footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Footer(){
+    return(
+        <div>
+            <button>Como funciona?</button>
+            <button>Quienes somos?</button>
+            <button>Medios de pago</button>
+        </div>
+    )
+}
---
+++ src/componentes/login.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+export default function login(){
+    return(
+        <div>
+            <img>logo</img>
+            <div>
+                    <form>
+                        <InputLabel htmlFor="name">Name:</InputLabel>
+                        <Input id="name" aria-actibeby="nombre" />
+                        <FormHelperText id="nombre">Tu nombre </FormHelperText>
+//----------------------- separador de elementos del formilario--------------------------------
+                        <InputLabel htmlFor="psw">Password:</InputLabel>
+                        <Input id="psw" type="password" aria-actibeby="password-helper" />
+                        <FormHelperText id="password-helper"></FormHelperText>
+//----------------------- separador de elementos del formilario--------------------------------
+                        <button>Login</button>
+                    </form>
+                <p>Haz click </p>
+                <link>aqui</link>
+                <p>si todavia no te has registrado</p>
+            </div>
+        </div>
+    )
+}
