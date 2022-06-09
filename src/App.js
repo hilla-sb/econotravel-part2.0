8a2f8fdee511a6de161d11bd2e6162e10a9cf3e6
mada <madaagarcia77@gmail.com>
2022-06-09 08:45:30 +0200

    renombrar carpetas en  minusculas

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import HomeNav from './componentes/nav/Navbar';
-import Home from './componentes/Home/Home';
+import Home from './componentes/home/Home';
 //import Destacados from './componentes/Home/Destacados'; 
 import Footer from './componentes/footer/Footer';
 
