95555bee843542447e939c4208c4533ea7bf8956
Lorelain <baldomerland@gmail.com>
2022-06-11 23:43:55 +0200

    cambios heroimg home: quitado handleclick de boton descubre más, cambio el alt de la imagen para quitar error

--- src/componentes/heroimage/HeroImg.js
+++ src/componentes/heroimage/HeroImg.js
@@ -1,18 +1,27 @@
-import React from 'react';
-import Button from 'react-bootstrap/Button';
-
-
+import React from "react";
+import { Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import Catalogo from "../../pages/Catalogo";
 
 /* import { Container } from 'react-bootstrap';
 import Button from 'react-bootstrap/Button'; */
 
-
-export default function Heroimg () {
+export default function Heroimg() {
+  
   return (
-
-      <div className="hero-img">
-            <img className="hero-image" src="https://images.unsplash.com/photo-1595954421407-ad5e012b25e5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1933&q=80" alt="Beautiful picture of Barcelona's Montjuic"/>
-            <Button className="descubre-mas" variant="secondary">Descubre más</Button>{' '}
-      </div>
-    )
-    }
+    <div className="hero-img">
+      <img
+        className="hero-image"
+        src="https://images.unsplash.com/photo-1595954421407-ad5e012b25e5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1933&q=80"
+        alt="Barcelona's Montjuic"
+      />
+      
+      <Link to={Catalogo}>
+        <Button className="descubre-mas" variant="secondary">
+          Descubre más
+        </Button>{" "}
+      </Link>
+      
+    </div>
+  );
+}
