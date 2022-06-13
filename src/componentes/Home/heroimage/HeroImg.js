358c6a69fc1cc5f95db24300fb33e66711e7ecab
mada <madaagarcia77@gmail.com>
2022-06-13 09:43:51 +0200

    haciendo commit para hacer un pull, error con herimag

--- src/componentes/Home/heroimage/HeroImg.js
+++ src/componentes/Home/heroimage/HeroImg.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Button from 'react-bootstrap/Button';
-
+import { Link } from 'react-router-dom';
 
 
 /* import { Container } from 'react-bootstrap';
@@ -12,7 +12,10 @@ export default function Heroimg () {
 
       <div className="hero-img">
             <img className="hero-image" src="https://images.unsplash.com/photo-1595954421407-ad5e012b25e5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1933&q=80" alt="Beautiful picture of Barcelona's Montjuic"/>
-            <Button className="descubre-mas" variant="secondary">Descubre más</Button>{' '}
+            <Link to="/catalogo">
+                <Button className="descubre-mas" variant="secondary">Descubre más</Button>{' '}
+            </Link>
+            
       </div>
     )
     }
