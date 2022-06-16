a5811b6828d79226df67bc2375f85ebe1a56dd81
julie <j.u.veiga@hotmail.com>
2022-06-16 19:56:53 +0200

    fix index

--- client/src/componentes/Detalle.js
+++ client/src/componentes/Detalle.js
@@ -1,10 +1,10 @@
-import { ListItem } from '@mui/material';
+//import { ListItem } from '@mui/material';
 import { render } from '@testing-library/react';
 import Breadcrumb from 'react-bootstrap/Breadcrumb';
 import detalle from '../experiencias.json';
 import { UseFetch } from './UseFetch';
 import React, {useState, useEffect} from 'react';
-import axios from "axios";
+//import axios from "axios";
 import { useParams } from "react-router-dom";
 
 //import Card from 'react-bootstrap/Card';
