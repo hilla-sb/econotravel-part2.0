6af6f01aec0487d1408858c482b3c424138934eb
mada <madaagarcia77@gmail.com>
2022-06-17 12:29:17 +0200

    estilos de catalogo, (y salio de pmt)

--- client/src/pages/Catalogo.js
+++ client/src/pages/Catalogo.js
@@ -1,33 +1,28 @@
-import React from 'react';
-import catalogo from '../experiencias.json';
-import { Link } from 'react-router-dom';
+import React from 'react'
+import catalogo from '../experiencias.json'
+import { Link } from 'react-router-dom'
+import { Button } from 'react-bootstrap'
 
-export default function Catalogo () {
+export default function Catalogo() {
+  let listaCatalogo = catalogo
 
-  let listaCatalogo = catalogo;
-  
   return listaCatalogo.map((el, i) => (
-  
-    
-    <div classname="catalogo">
-  
-      <div>
+    <div className="catalogo">
       <Link to={`/detalle/${el.id}`}>
-        <img width={"400px"} heigh={"600px"} src={el.img}/>
+        <img width={'400px'} heigh={'600px'} className="imagenDestacados" src={el.img} />
       </Link>
-        <div>
-          <button >{el.eti1}</button>
-          <button >{el.eti2}</button>
-          <button >{el.eti3}</button>
-        </div>
+      <div className="etiquetasDetalle">
+        <Button variant="secondary">{el.eti1}</Button>
+        <Button variant="secondary">{el.eti2}</Button>
+        <Button variant="secondary">{el.eti3}</Button>
+      </div>
+      <div className='miniDescripcion'>
         <div>
           <p>{el.eti1}</p>
           <p>{el.precio}</p>
         </div>
-        <Link to='/reserva'>Reserva ahora</Link>
+        <Link to="/reserva" className='reserva'>Reserva ahora</Link>
       </div>
-  
-    
     </div>
   ))
 }
