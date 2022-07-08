2b0ff377e2397e3b88022ebbaded0cea07758caf
hilla-sb <hillsb2302@gmail.com>
2022-07-08 11:37:29 +0200

    #1-conexion base de datos,creacion carpeta service config.ts,database service

---
+++ server/src/service/database.service.ts
@@ -0,0 +1,18 @@
+import { config } from './config'
+import { Client } from 'pg'
+const url: any = config();
+export const connection = (queryStr:string, values:string[]=[]) =>{
+    try{
+        const client = new Client (url);
+        console.log('Connected successfully to server');
+        return client.query(queryStr,values);
+    } catch (error){
+        console.log(error)
+    }
+}
+
+
+
+
+
+
