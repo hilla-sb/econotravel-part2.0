ab93096c8f0e1d3fb1d8f510de3c27667058fbdd
Marielle <marielle.ibias@example.com>
2022-07-13 10:47:19 +0200

    #1 actualizo app con usuarioRoute

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,9 +1,11 @@
 import express, {Request, Response} from 'express';
 import experienciaRoute from './route/experienciaRoute';
+import usuarioRoute from './route/usuarioRoute'
 
 const app = express(); 
 app.use(express.json());
 app.use(experienciaRoute);
+app.use(usuarioRoute);
 
 app.get('/',(req:Request, res:Response)=>{
     res.json('hello Word');
