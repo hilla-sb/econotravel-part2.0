ff0cd387697898e28f56092ebec0c215d2ff5174
hilla-sb <hillsb2302@gmail.com>
2022-07-18 14:44:18 +0200

    #7 base de usuarios ,incorporacion de file usuarioModel,reserva Controller,ireserva

--- server/src/app.ts
+++ server/src/app.ts
@@ -2,12 +2,14 @@ import express, {Request, Response} from 'express';
 import experienciaRoute from './route/experienciaRoute';
 import usuarioRoute from './route/usuarioRoute';
 import loginRoute from './route/loginRoute';
+import reservaRoute from './route/reservaRoute';
 
 const app = express(); 
 app.use(express.json());
 app.use(experienciaRoute);
 app.use(usuarioRoute);
 app.use(loginRoute);
+app.use(reservaRoute);
 
 app.get('/',(req:Request, res:Response)=>{
     res.json('hello World');
---
+++ server/src/model/reservaModel.ts
@@ -0,0 +1,31 @@
+import { connection } from "../services/database.service";
+import { Client } from 'pg';
+
+class Reserva {
+    client: Client;
+    constructor(client:any) {
+      this.client = client
+    }
+    async getReservas(){
+        const queryStr= 'SELECT * FROM reserva'
+        await this.client.connect()
+        const resultado = await this.client.query(queryStr);
+        await this.client.end()
+        return resultado.rows;
+    }
+    async getUnaReserva(id_reserva:number){
+        const queryStr='SELECT * FROM reserva WHERE id=$1'
+        await this.client.connect()
+        const resultado = await this.client.query(queryStr,[id_reserva]);
+        await this.client.end()
+        return resultado.rows;
+    }
+    async deleteReserva (id_reserva:number){
+        const queryStr='DELETE FROM reserva WHERE id_reserva = $1 returning *'
+        const resultado = await this.client.query(queryStr,[id_reserva]);
+        await this.client.end()
+        return resultado.rows;
+    }
+}
+
+export default new Reserva(connection());
