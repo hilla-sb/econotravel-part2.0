c6864f3e4663cb23fd7502bdd40328568c7ffd44
Marielle <marielle.ibias@example.com>
2022-07-26 14:57:55 +0200

    #3 editExperiencia working

--- server/src/controller/exp_controllers/editController.ts
+++ server/src/controller/exp_controllers/editController.ts
@@ -7,7 +7,7 @@ const editController = async (req: Request, res: Response,) => {
     try {
         const id:any = req.params.id
         const experiencia:iExperiencia = req.body
-
+        
         const resultado:iExperiencia = 
         await experienciaModel.editExperiencia(id,experiencia);
             res.status(200).json({ message: `La experiencia ${resultado.id_experiencia} ha sido editada con éxito.` })
