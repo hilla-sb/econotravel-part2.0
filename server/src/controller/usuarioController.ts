f8c0502af32f5a66eb152ddb76be1f1c64310f27
julie <j.u.veiga@hotmail.com>
2022-07-26 09:03:34 +0200

    #8 update usuarioController

--- server/src/controller/usuarioController.ts
+++ server/src/controller/usuarioController.ts
@@ -5,7 +5,7 @@ import usuarioModel from '../model/usuarioModel';
 const usuarioController = async (req: Request, res: Response) => {
   const { email, password }: iUsuario = req.body;
   if (!email) {
-    res.status(400).json({ message: 'email or password missing' });
+    res.status(400).json({ message: 'Porfavor, rellene todos los campos obligatorios.' });
   }
   const result = await usuarioModel.saveUsuario({ email, password });
 
