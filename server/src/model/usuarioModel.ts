7bfdf95572d1154015a9bd9bff6c694e81867744
Marielle <marielle.ibias@example.com>
2022-07-27 20:19:41 +0200

    #8 #9 #10 #12 userModel updated

--- server/src/model/usuarioModel.ts
+++ server/src/model/usuarioModel.ts
@@ -1,38 +1,56 @@
 import { connection } from "../services/database.service";
 import { iUsuario, iUsuariologin } from "./interfaces/iUsuario";
-import { Client } from "pg";
+import { Pool } from "pg";
 
 class Usuario {
-  client: Client;
+  client: Pool;
   constructor(client: any) {
     this.client = client;
   }
 
-  async getUsuario (usuario: iUsuariologin) {
-    const queryStr = 'SELECT * FROM usuario WHERE email = $1';
-    const values = [usuario.email];
-    await this.client.connect();
-    console.log('conectado')
-    const resultado = await this.client.query(queryStr, values);
-    console.log('query ok')
-    // await this.client.end();
-    return resultado.rows[0];
+  async getUsuarios() {
+    try {
+      const queryStr = 'SELECT * FROM usuario'
+      const resultado: any = await this.client.query(queryStr,[]);
+      return resultado.rows;
+    } catch (error) {
+      console.log(error);
+    }
   }
 
-  async saveUsuario(usuario: iUsuario) {
+  async getUnUsuario(usuario: iUsuariologin) {
+    try {
+      const queryStr = 'SELECT * FROM usuario WHERE email = $1';
+      const values = [usuario.email];
+      const resultado = await this.client.query(queryStr, values);
+      return resultado.rows;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async saveUsuario(usuario:iUsuario) {
     try {
       const queryStr =
-        'INSERT INTO "usuario"(email,password,nombre) VALUES($1,$2,$3) RETURNING *';
-      const values = [usuario.email, usuario.password, usuario.nombre || null];
-      /*  const cliente = await connection(); */
-      await this.client.connect();
+        'INSERT INTO "usuario"(email, password, nombre, apellidos) VALUES($1,$2, $3, $4) RETURNING *';
+      const values = [usuario.email, usuario.password, usuario.nombre, usuario.apellidos || null];
       const resultado = await this.client.query(queryStr, values);
-      await this.client.end();
       return resultado.rows[0];
     } catch (error) {
       console.log(error);
     }
   }
 
+  async loginUser(email: string, password:string) {
+    try {
+      const queryStr =
+        'SELECT * FROM usuarios WHERE email = $1'
+      const resultado = await this.client.query(queryStr, [email, password]);
+      console.log(resultado.rows)
+      return resultado.rows[0];
+    } catch (error) {
+      console.log(error);
+    }
+  }
 }
 export default new Usuario(connection());
--- server/src/route/usuarioRoute.ts
+++ server/src/route/usuarioRoute.ts
@@ -2,14 +2,15 @@ import { Router } from "express";
 import authHandler from "../middleware/authHandler";
 import jwtHandler from "../middleware/jwtHandler";
 import usuarioController from "../controller/usuarioController";
+import loginController from "../controller/loginController";
 
 const router = Router();
 
 router.get("/usuarios/", usuarioController);
 
-// router.post('/usuario/register', authHandler.encryptPassword, usuarioController.saveUsuario)
+router.post('/usuario/register', )
 
-// router.post("/usuarios/login", authHandler.validateUser, usuarioController);
+router.post("/usuarios/login", loginController);
 
 
 export default router;
